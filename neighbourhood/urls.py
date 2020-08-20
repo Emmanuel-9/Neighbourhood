fb56b70254cc90fdd0af38370fb9c52c9057a928
Vincent <chridtinewanja@gmail.com>
2020-08-20 09:40:50 +0300

    This adds the urls

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -16,6 +16,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+
+urlpatterns=[
+    url(r'^$',views.index,name = 'index'),    
+    url(r'^sendemail/$',views.send_email,name = 'send-email'),
+    url(r'^create-admin/$',views.create_admin,name = 'create-admin'),
+    url(r'^create-hood/$',views.create_hood,name = 'create-hood'),
+    url(r'^update-hood/$',views.update_hood,name = 'update-hood'),
+    url(r'^admin-profile/$',views.admin_profile,name = 'admin-profile'),
+    url(r'^user-profile/$',views.user_profile,name = 'user-profile'),
+    url(r'^add-occupant/$',views.add_resident,name = 'add-occupant'),
+    url(r'^delete-hood/$',views.delete_hood,name = 'delete-hood'),
+    url(r'^add-amenity/$',views.add_amenity,name = 'add-amenity'),
+    url(r'^add-business/$',views.add_business,name = 'add-business'),
+    url(r'^delete-occupant-profile/$',views.delete_resident_profile,name = 'delete-occupant-profile'),
+    url(r'^change-password/$',views.change_password,name = 'change-password'),
+    url(r'^make-post/$',views.make_post,name = 'make-post'),
+    url(r'^occupants-list/$',views.residents_list,name = 'occupants-list'),
+    url(r'^delete-occupant/(\d+)',views.delete_resident,name = 'delete-occupant'),
+    url(r'^changeprofilephoto/$',views.change_profile_photo,name = 'change-profile-photo'),
 ]
+git

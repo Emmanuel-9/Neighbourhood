0fa068ea2fbc4551ad806cc85d14d543dab4ea73
Joan_E <joanevans18@gmail.com>
2020-08-20 08:59:57 +0300

    finished user authentication

---
+++ users/foms.py
@@ -0,0 +1,28 @@
+from django.forms import ModelForm, Form
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import UserProfile
+from django import forms
+
+
+
+class SignUpForm(UserCreationForm):    
+
+    class Meta:
+        model = User
+        fields = ['username','email', 'password1', 'password2'] 
+
+class UserUpdateForm(ModelForm):
+    email = forms.EmailField(max_length=150)
+
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class ProfileUpdateForm(ModelForm):
+    bio = forms.CharField()
+
+    class Meta:
+        model = UserProfile
+        fields = ['image', 'bio']

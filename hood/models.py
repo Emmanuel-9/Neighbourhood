c1b10e7d6a6f699e2290c8c9145683915ac9d327
Joan_E <joanevans18@gmail.com>
2020-08-20 09:00:41 +0300

    created another app hood with business and neighbourhood models

---
+++ hood/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, null=True)
+    occ_count = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return f'{self.name}'
+

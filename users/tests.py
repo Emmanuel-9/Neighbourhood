a5d88fa7598da25fe07ecc9bb58291884fd0095b
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-21 11:14:19 +0300

    Add the UserProfileTest class.

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from models import UserProfile
 
 # Create your tests here.
+class UserProfileTest(TestCase):
+    def setUp(self):
+        self.user1 = UserProfile(user='User1',bio="Fake till you make it",image='default.jpg',contact='07456390037')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user1,UserProfile))
+
+    

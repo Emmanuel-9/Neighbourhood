e6bf1f17c6f398d071d03efa5c0b54a26e3b4bd1
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-21 11:11:33 +0300

    Add the classes BusinessTest and NeighbourhoodTest.

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from models import Business,Neighbourhood
 
 # Create your tests here.
+class BusinessTest(TestCase):
+    def setUp(self):
+        self.real_estate = Business(name='Agribusiness',user='Martin',neighbourhood='Eastlands',email='martin@info.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.real_estate,Business))
+
+
+class NeighbourhoodTest(TestCase):
+    def setUp(self):
+        self.juja = Neighbourhood(name='Juja',location='Kiambu',occ_count=4)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.juja,Neighbourhood))        
